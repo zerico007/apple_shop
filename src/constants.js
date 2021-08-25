0954c65c9810e06626dc58270ebdddc0b01aee49
Bavin Edwards <65621465+zerico007@users.noreply.github.com>
2021-08-25 07:11:27 -0500

    changed constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://apple-shop-backend.herokuapp.com/api";
-export const BASE_URL = "http://localhost:4000/api";
+export const BASE_URL = "https://apple-shop-backend.herokuapp.com/api";
+//export const BASE_URL = "http://localhost:4000/api";
 
-// export const IMAGES_URL = "https://apple-shop-backend.herokuapp.com";
-export const IMAGES_URL = "http://localhost:4000";
+export const IMAGES_URL = "https://apple-shop-backend.herokuapp.com";
+// export const IMAGES_URL = "http://localhost:4000";
